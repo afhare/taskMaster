57df780c489cf90fed56100387b28a2bb6ce5778
afhare <alexandra.f.hare@gmail.com>
2019-09-22 20:23:29 -0500

    Initialized project

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+import HomeContainer from './containers/HomeContainer';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>TaskMaster</h1>
+      <HomeContainer />
+    </div>
+  );
+}
+
+export default App;
